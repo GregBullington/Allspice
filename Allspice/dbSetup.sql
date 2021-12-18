ba3f0e6f6e06d0e9a0024a9260e3dbfd8b0a49ce
Greg Bullington <gbullington23@gmail.com>
2021-12-18 02:12:59 -0700

    Table-Fix

--- Allspice/dbSetup.sql
+++ Allspice/dbSetup.sql
@@ -6,4 +6,13 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+CREATE TABLE recipes(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    title VARCHAR(255) NOT NULL,
+    subTitle VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    creatorId VARCHAR(255) NOT NULL,
+    FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) DEFAULT CHARSET UTF8 COMMENT '';
+
 
