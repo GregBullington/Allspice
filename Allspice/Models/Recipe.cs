a26d0aab2d0dc979b4bf5c0cbe34c35be4a35a6c
Greg Bullington <gbullington23@gmail.com>
2021-12-18 01:19:49 -0700

    Recipe-Setup-ServerSide

---
+++ Allspice/Models/Recipe.cs
@@ -0,0 +1,12 @@
+namespace Allspice.Models
+{
+  public class Recipe
+  {
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Subtitle { get; set; }
+    public string Category { get; set; }
+    public string CreatorId { get; set; }
+
+  }
+}
